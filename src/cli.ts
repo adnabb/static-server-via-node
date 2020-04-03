8d62349400b6b087b3d156c7980cdfe3fa303a30
chenshiyu <chenshiyu@ygsoft.com>
2020-04-03 18:04:08 +0800

    feat:新增console color+命令行可以输入路径启动服务

--- src/cli.ts
+++ src/cli.ts
@@ -7,5 +7,9 @@ const { program } = commander;
 program
   .version(version)
   .name('static');
-if (process.argv.length === 2) { createServer(); }
+if (process.argv.length === 2) {
+  createServer();
+} else if (process.argv.length === 3) {
+  createServer(process.argv[2]);
+}
 program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import * as http from 'http';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as colors from 'colors';
 import mimeTypeMap from './mimeTypeMap';
 
+let _root:string;
 const port = 8089;
 const host = 'localhost';
-const root = 'public';
 
 const getRequestCompletePath = (request: http.IncomingMessage) => {
   let { pathname } = new URL(request.url, `http://${request.headers.host}`);
@@ -15,37 +16,76 @@ const getRequestCompletePath = (request: http.IncomingMessage) => {
 
 const getFileMimeAndPath = (pathname: string) => {
   let fileType = pathname.match(/\..*/) ? pathname.match(/\..*/)[0] : '';
+  // default fileType: .html
   if (!(fileType in mimeTypeMap)) fileType = '.html';
   const { mime, path: filePath } = mimeTypeMap[fileType];
-  const readFilePath = filePath ? path.join(root, filePath, pathname) : path.join(root, pathname);
+  const readFilePath = filePath ? path.join(_root, filePath, pathname) : path.join(_root, pathname);
 
   return { mime: mime, path: readFilePath, };
 };
 
+const getRootPath = (customPath:string) => {
+  if (!customPath) {
+    return process.cwd();
+  } else if (path.isAbsolute(customPath)) {
+    return customPath;
+  }
+
+  return path.join(process.cwd(), customPath);
+}
+
+const handleNotGetRequest = (response:http.ServerResponse) => {
+  response.statusCode = 405;
+  response.end();
+}
+
+const get404HTML = () => fs.readFileSync(path.resolve(__dirname, '../public', '404.html'));
+
+const setConsoleColorTheme = () => {
+  colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+  });
+};
+
 let _server:http.Server;
 
-const createServer = () => {
+const createServer = (customPath?:string) => {
+   _root = getRootPath(customPath);
+   setConsoleColorTheme();
+
   _server = http.createServer((request, response) => {
-    if (request.method !== 'GET') { response.statusCode = 405; response.end(); return; }
-  
+    if (request.method !== 'GET') { handleNotGetRequest(response); return; }
+
     const requestUrl = getRequestCompletePath(request);
     const { path: readFilePath, mime } = getFileMimeAndPath(requestUrl);
     let statusCode = 200;
     fs.readFile(readFilePath, (error, data) => {
       if (error) {
-        console.error('error', error);
+        // @ts-ignore
+        console.error('error'.error, error);
         statusCode = 404;
-        data = fs.readFileSync(path.join(root, '404.html'));
+        data = get404HTML();
       }
       response.writeHead(statusCode, {
         'Content-Type': mime,
         'Content-Length': data.length,
-      }).end(data);
+      });
+      response.end(data);
     });
   });
 
   _server.listen(port, host, () => {
-    console.log(`you are listening http://${host}:${port}`);
+    // @ts-ignore
+    console.info(`you are listening http://${host}:${port}`.info);
   });
 }
 
