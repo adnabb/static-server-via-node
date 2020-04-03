e4e9545bd1164ec7e073b6dd1efab21539e1da0c
chenshiyu <chenshiyu@ygsoft.com>
2020-04-03 09:59:39 +0800

    feat:可以识别任意文件+加入404和405处理

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
-import * as http from "http";
+import * as http from 'http';
+import * as fs from 'fs';
+import * as path from 'path';
+import mimeTypeMap from './mimeTypeMap';
 
 const port = 8089;
 const host = 'localhost';
+const root = 'public';
+let notFoundPage: Buffer;
+
+const getRequestCompletePath = (request: http.IncomingMessage) => {
+  let { pathname } = new URL(request.url, `http://${request.headers.host}`);
+  if (pathname === '/') pathname = '/index.html';
+  return pathname;
+}
+
+const getFileMimeAndPath = (pathname:string) => {
+  const fileType = pathname.match(/\..*/) ? pathname.match(/\..*/)[0] : '.html';
+  const { mime, path: filePath } = mimeTypeMap[fileType];
+  const readFilePath = filePath ? path.join(root, filePath, pathname) : path.join(root, pathname);
+
+  return { mime: mime, path: readFilePath, };
+};
+
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('hi');
+  if (request.method !== 'GET') { response.statusCode = 405; response.end(); return; }
+
+  const requestUrl = getRequestCompletePath(request);
+  const { path: readFilePath, mime } = getFileMimeAndPath(requestUrl);
+  fs.readFile(readFilePath, (error, data) => {
+    if (error) {
+      console.error('error', error);
+      response.statusCode = 404;
+      notFoundPage = fs.readFileSync(path.join(root, '404.html'));
+    }
+    response.setHeader('Content-Type', mime);
+    response.end(data || notFoundPage);
+  });
 });
 
 server.listen(port, host, () => {
---
+++ src/mimeTypeMap.ts
@@ -0,0 +1,33 @@
+export default ({
+  '.html': {
+    mime: 'text/html',
+  },
+  '.css': {
+    mime: 'text/css',
+    path: 'css',
+  },
+  '.js': {
+    mime: 'application/javascript',
+    path: 'js',
+  },
+  '.jpg': {
+    mime: 'image/jpeg',
+    path: 'images',
+  },
+  '.jpeg': {
+    mime: 'image/jpeg',
+    path: 'images',
+  },
+  '.png': {
+    mime: 'image/png',
+    path: 'images',
+  },
+  '.gif': {
+    mime: 'image/gif',
+    path: 'images',
+  },
+  '.ico': {
+    mime: 'image/x-icon',
+    path: 'images',
+  },
+});
